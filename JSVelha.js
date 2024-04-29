19f483682b3bd5e5780126a4402c127b91fcbc39
CavalheiroOtavioSENAI <163847004+otaviocesr@users.noreply.github.com>
2024-04-29 15:01:13 -0300

    Arquivo JavaScript-Velha

---
+++ JSVelha.js
@@ -0,0 +1,121 @@
+﻿function main() {
+    while (true) {
+        principal2();
+    }
+}
+
+function principal2() {
+    // Criar tabuleiros e jogadores. Zerar as variáveis
+    // 
+    // 0: Posição Vazia
+    // 1: Jogada na posição do Jogador1
+    // 2: Jogada na posição do Jogador2
+    var velha;
+    var linha;
+    var coluna;
+    var haVencedor;
+
+    haVencedor = 0;
+    var tabuleiro = createArray(9);
+
+    // Limpar/Zerar o tabuleiro
+    var index;
+
+    for (index = 0; index <= 8; index++) {
+        tabuleiro[index] = 0;
+    }
+    var jogada;
+
+    // Iniciar o jogo, definir quem joga primeiro
+    var jogadorDavez;
+
+    jogadorDavez = 1;
+
+    // Anotar/Registrar a jogada, do primeiro jogador
+    velha = 1;
+    do {
+        window.alert(tabuleiro[0].ToString() + tabuleiro[1] + tabuleiro[2]);
+        window.alert(tabuleiro[3].ToString() + tabuleiro[4] + tabuleiro[5]);
+        window.alert(tabuleiro[6].ToString() + tabuleiro[7] + tabuleiro[8]);
+        jogada = "";
+        window.alert("Digite a posição da sua peça jogador " + jogadorDavez);
+        jogada = window.prompt('Enter a value for jogada');
+        if (varrerJogada(jogada) == true) {
+
+            // Simula a função Serial.parseInt do Arduino
+            linha = parseInt(jogada.charAt(0));
+            coluna = parseInt(jogada.charAt(2));
+
+            // Verificar se a posição "jogada" é valida
+            // Converter a jogada texto em dois inteiros linha e coluna
+            if (tabuleiro[3 * linha + coluna] == 0) {
+                tabuleiro[3 * linha + coluna] = jogadorDavez;
+                haVencedor = verificacao(tabuleiro, jogadorDavez);
+                if (jogadorDavez == 1) {
+                    jogadorDavez = 2;
+                } else {
+                    jogadorDavez = 1;
+                }
+
+                // Verificar o tabuleiro, se houve ganhador ou empate, finalizar o jogo
+                // Verificar a jogada vencedora nas linhas
+                velha = velha + 1;
+            } else {
+                window.alert("Erro. Posição já ocupada, digite novamente outra posição");
+
+                // Informar ao Jogador 1 que a posição está preenchida, é inválida e ele precisa informar uma posição válida.
+            }
+        } else {
+            window.alert("JOGADA INVÁLIDA");
+        }
+    } while (haVencedor == 0 && velha <= 9);
+    if (jogadorDavez == 1) {
+        jogadorDavez = 2;
+    } else {
+        jogadorDavez = 1;
+    }
+    if (haVencedor == 1) {
+        window.alert("Parabéns, você ganhou jogador " + jogadorDavez);
+    } else {
+        window.alert("Deu Velha!");
+    }
+}
+
+function varrerJogada(entrada) {
+    var entradaValida;
+
+    entradaValida = false;
+    window.alert(entrada.length());
+    if (entrada.length() == 3) {
+        if (entrada.charAt(0) == "0" || entrada.charAt(0) == "1" || entrada.charAt(0) == "2") {
+            if (entrada.charAt(2) == "0" || entrada.charAt(2) == "1" || entrada.charAt(2) == "2") {
+                entradaValida = true;
+            }
+        }
+    }
+    
+    return entradaValida;
+}
+
+function verificacao(tabuleiro, jogadorDaVez) {
+    var vencedor;
+
+    vencedor = 0;
+    if (tabuleiro[0] == jogadorDaVez && tabuleiro[1] == jogadorDaVez && tabuleiro[2] == jogadorDaVez || tabuleiro[3] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[5] == jogadorDaVez || tabuleiro[6] == jogadorDaVez && tabuleiro[7] == jogadorDaVez && tabuleiro[8] == jogadorDaVez) {
+        vencedor = 1;
+    } else {
+
+        // Verificar a jogada vencedora nas colunas
+        if (tabuleiro[0] == jogadorDaVez && tabuleiro[3] == jogadorDaVez && tabuleiro[6] == jogadorDaVez || tabuleiro[1] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[7] == jogadorDaVez || tabuleiro[2] == jogadorDaVez && tabuleiro[5] == jogadorDaVez && tabuleiro[8] == jogadorDaVez) {
+            vencedor = 1;
+        } else {
+
+            // Verificar a jogada vencedora nas dioganais
+            if (tabuleiro[0] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[8] == jogadorDaVez || tabuleiro[2] == jogadorDaVez && tabuleiro[4] == jogadorDaVez && tabuleiro[6] == jogadorDaVez) {
+                vencedor = 1;
+            }
+        }
+    }
+    
+    return vencedor;
+}
